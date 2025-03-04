0652a14846fa47aaf62b0b1cbaeca091b0768b7f
melvstein <melvinbayogo@gmail.com>
2025-03-05 04:41:33 +0800

    ecommerce mj store

--- src/app/customer/login/page.tsx
+++ src/app/customer/login/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 
 import GoogleSignin from "@/components/GoogleSignin";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 
 const SignIn: React.FC = () => {
 	const { data: session } = useSession();
 	const router = useRouter();
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
 
 	useEffect(() => {
         if (session?.user) {
@@ -15,19 +17,35 @@ const SignIn: React.FC = () => {
         }
     }, [session, router]);
 
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const result = signIn("credentials", {
+			redirect: false,
+			email,
+			password
+		});
+
+		console.log(result);
+
+		//router.push("/");
+	}
+
 	return (
-		<section className="flex items-center justify-center text-skin-muted">
+		<section className="flex items-center justify-center text-skin-muted m-4">
 			<div className="flex flex-col items-center justify-center p-4 rounded-lg border shadow-lg w-[500px] gap-y-4 mt-[80px]">
 				<div>
 					<h1>Customer Login</h1>
 				</div>
 				<GoogleSignin />
 				<p>or</p>
-				<input type="text" placeholder="Email or Username" className="input-skin" />
-				<input type="password" placeholder="Password" className="input-skin" />
-				<button className="border shadow rounded-lg px-4 py-2 focus:outline-2 outline-skin-primary bg-skin-primary text-skin-base w-full font-semibold">
-					Login
-				</button>
+				<form className="space-y-4" onSubmit={handleSubmit}>
+					<input type="text" placeholder="Email or Username" className="input-skin" value={email} onChange={(e) => setEmail(e.target.value)} />
+					<input type="password" placeholder="Password" className="input-skin" value={password} onChange={(e) => setPassword(e.target.value)} />
+					<button className="border shadow rounded-lg px-4 py-2 focus:outline-2 outline-skin-primary bg-skin-primary text-skin-base w-full font-semibold">
+						Login
+					</button>
+				</form>
 			</div>
 		</section>
 	)
